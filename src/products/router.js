c1e34d466f8aca4e96770d3cddef36f60d29514d
Luis Ho <evil_luis@hotmail.com>
2020-08-16 17:52:33 -0400

    working sample

--- src/products/router.js
+++ src/products/router.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const config = require('../config.js');
 const cors = require('cors');
 
+const data = require('../samples/products.json');
+
 router.all('*', cors());
 
 function getProducts (req, res, next) {
-  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+  res.json(data);
 }
 
 router.get('/products', getProducts)
