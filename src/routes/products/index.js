2f03658948721090cb0093c32380c29556163811
Luis Ho <evil_luis@hotmail.com>
2020-09-28 22:04:35 -0400

    add update script

--- src/routes/products/index.js
+++ src/routes/products/index.js
@@ -44,6 +44,49 @@ router.delete('/products', (req, res, next) => {
   })
 });
 
+router.put('/products/:id', (req, res, next) => {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err)
+    } else if (err) {
+      return res.status(500).json(err)
+    }
+    const body = req.body;
+    const pid = req.params.id;
+
+    Product.find({ where: { id: pid } })
+    .on('success', function (product) {
+      // Check if record exists in db
+      if (product) {
+        product.update({
+          'name': body.name,
+          'stock': body.stock,
+          'amount': body.amount,
+          'category': body.category,
+          'brand': body.brand,
+          'model': body.model,
+          'code': body.code,
+          'description': body.description,
+          'vendor': body.vendor,
+        })
+        .success(function (new_product) {
+          let counter = 1;
+          const newImages = req.files.map((data) => {
+            return {
+              'productId': new_product.id,
+              'img_url': data.filename,
+              'position': counter++
+            }
+          })
+          ProductImages.bulkCreate(newImages).then((images) => {
+            res.status(200).json(new_product);
+          })
+        })
+      }
+    })
+  })
+});
+
 router.post('/products', (req, res, next) => {
   // add / update products
   upload(req, res, function (err) {
