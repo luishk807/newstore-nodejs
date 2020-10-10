e78bba70dd6a2bedf62592671b278bc78ff15798
luishk807 <luishk807@hotmail.com>
2020-10-10 09:15:22 -0400

    more update on config

--- src/config.js
+++ src/config.js
@@ -6,6 +6,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 config.express = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 5000,
   whitelist: ['http://localhost:3000','http://localhost:8080','https://avenidaz.herokuapp.com/'],
 }
