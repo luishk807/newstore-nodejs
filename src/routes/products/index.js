6696ef0adaf27169af82eb2158c27b8283d190f7
luishk807 <luishk807@hotmail.com>
2020-11-09 12:33:32 -0500

    update vendor product routes

--- src/routes/products/index.js
+++ src/routes/products/index.js
@@ -238,9 +238,18 @@ router.get('/products/:id', async(req, res, next) => {
 router.get('/products', async(req, res, next) => {
   // get products
   let product = null;
+  
   if (req.query.id) {
     try {
       product = await Product.findOne({ where: {id: req.query.id}, include: ['productStatus', 'productImages']});
+
+      res.json(product)
+    } catch(err) {
+      res.send(err)
+    }
+  } else if (req.query.vendor) {
+    try {
+      product = await Product.findAll({ where: {vendorId: req.query.vendor}, include: ['productStatus', 'productImages']});
 
       res.json(product)
     } catch(err) {
@@ -256,113 +265,113 @@ router.get('/products', async(req, res, next) => {
   }
 });
 
-router.get('/productsvendor', [verify], async(req, res, next) => {
-  // get products
-  let product = null;
-  if (req.query.id) {
-    try {
-      product = await Product.findAll({ where: {vendor: req.query.id}, include: ['productStatus', 'productImages', 'productVendor']});
+// router.get('/productsvendor', [verify], async(req, res, next) => {
+//   // get products
+//   let product = null;
+//   if (req.query.id) {
+//     try {
+//       product = await Product.findAll({ where: {vendor: req.query.id}, include: ['productStatus', 'productImages', 'productVendor']});
 
-      res.json(product)
-    } catch(err) {
-      res.send({status: false, message: err})
-    }
-  } else {
-      res.send({status: false, message: "ERROR: invalid vendor"});
-  }
-});
+//       res.json(product)
+//     } catch(err) {
+//       res.send({status: false, message: err})
+//     }
+//   } else {
+//       res.send({status: false, message: "ERROR: invalid vendor"});
+//   }
+// });
 
-router.post('/productsvendor', [verify, upload], (req, res, next) => {
-  // add / update products
+// router.post('/productsvendor', [verify, upload], (req, res, next) => {
+//   // add / update products
 
-  const imagesUploaded = req.files.map((file) => {
-    let myFile = file.originalname.split('.');
-    const fileType = myFile[myFile.length - 1];
-    const fileName = `${uuid.v4()}.${fileType}`;
-    const params = {
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: fileName,
-      Body: file.buffer,
-    }
+//   const imagesUploaded = req.files.map((file) => {
+//     let myFile = file.originalname.split('.');
+//     const fileType = myFile[myFile.length - 1];
+//     const fileName = `${uuid.v4()}.${fileType}`;
+//     const params = {
+//       Bucket: process.env.AWS_BUCKET_NAME,
+//       Key: fileName,
+//       Body: file.buffer,
+//     }
 
-    s3.upload(params, (err, data) => {
-      if (err) {
-        res.status(500).send(err)
-      }
-    })
+//     s3.upload(params, (err, data) => {
+//       if (err) {
+//         res.status(500).send(err)
+//       }
+//     })
 
-    return {
-      Key: fileName
-    };
-  })
+//     return {
+//       Key: fileName
+//     };
+//   })
 
-  const body = req.body;
-  Product.create(
-    {
-      'name': body.name,
-      'stock': body.stock,
-      'amount': body.amount,
-      'category': body.category,
-      'brand': body.brand,
-      'model': body.model,
-      'code': body.code,
-      'description': body.description,
-      'vendor': body.vendor,
-    }
-  ).then((product) => {
-    let counter = 1;
-    const newImages = imagesUploaded.map((data) => {
-      return {
-        'productId': product.id,
-        'img_url': data.Key,
-        'position': counter++
-      }
-    })
-    ProductImages.bulkCreate(newImages).then((images) => {
-      res.status(200).json({status: true, message: "Product added", data: product});
-    })
-  }).catch(err => {
-    res.status(401).json({status: false, message: "Unable to add product"});
-  })
-})
+//   const body = req.body;
+//   Product.create(
+//     {
+//       'name': body.name,
+//       'stock': body.stock,
+//       'amount': body.amount,
+//       'category': body.category,
+//       'brand': body.brand,
+//       'model': body.model,
+//       'code': body.code,
+//       'description': body.description,
+//       'vendor': body.vendor,
+//     }
+//   ).then((product) => {
+//     let counter = 1;
+//     const newImages = imagesUploaded.map((data) => {
+//       return {
+//         'productId': product.id,
+//         'img_url': data.Key,
+//         'position': counter++
+//       }
+//     })
+//     ProductImages.bulkCreate(newImages).then((images) => {
+//       res.status(200).json({status: true, message: "Product added", data: product});
+//     })
+//   }).catch(err => {
+//     res.status(401).json({status: false, message: "Unable to add product"});
+//   })
+// })
 
-router.delete('/productsvendor/:id', verify, (req, res, next) => {
-  // delete products
-  Product.findAll({ where: {id: req.params.id},include:['productImages','productVendor', 'productBrand', 'categories', 'productStatus', 'rates']})
-  .then((product) => {
-    const mapFiles = product[0].productImages.map(data => {
-      return data.img_url;
-    })
-    Product.destroy({
-      where: {
-        id: product[0].id
-      }
-    }).then((deletedRecord) => {
-      if (deletedRecord) {
-        // console.log(deletedRecord, mapFiles)
-        try {
-          mapFiles.forEach(data => {
-            console.log(data)
-            const params = {
-              Bucket: process.env.AWS_BUCKET_NAME,
-              Key: data,
-            }
-            s3.deleteObject(params, (err, data) => {
-              if (err) {
-                res.status(500).send({status: false, message: err})
-              }
-            })
-          })
-          res.status(200).json({ status: true, message: "Product successfully deleted" });
-        } catch (e) {
-          res.status(400).json({ status: false, message: "Product delete, but error on deleting image!", error: e.toString(), req: req.body });
-        }
-      }
-    }, (err) => {
-        res.json(err);
-    })
-  })
-});
+// router.delete('/productsvendor/:id', verify, (req, res, next) => {
+//   // delete products
+//   Product.findAll({ where: {id: req.params.id},include:['productImages','productVendor', 'productBrand', 'categories', 'productStatus', 'rates']})
+//   .then((product) => {
+//     const mapFiles = product[0].productImages.map(data => {
+//       return data.img_url;
+//     })
+//     Product.destroy({
+//       where: {
+//         id: product[0].id
+//       }
+//     }).then((deletedRecord) => {
+//       if (deletedRecord) {
+//         // console.log(deletedRecord, mapFiles)
+//         try {
+//           mapFiles.forEach(data => {
+//             console.log(data)
+//             const params = {
+//               Bucket: process.env.AWS_BUCKET_NAME,
+//               Key: data,
+//             }
+//             s3.deleteObject(params, (err, data) => {
+//               if (err) {
+//                 res.status(500).send({status: false, message: err})
+//               }
+//             })
+//           })
+//           res.status(200).json({ status: true, message: "Product successfully deleted" });
+//         } catch (e) {
+//           res.status(400).json({ status: false, message: "Product delete, but error on deleting image!", error: e.toString(), req: req.body });
+//         }
+//       }
+//     }, (err) => {
+//         res.json(err);
+//     })
+//   })
+// });
 
 
 module.exports = router
