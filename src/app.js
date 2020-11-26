1c4ddfc67b5650a8788f0d11afb4654fa02c8c2d
luishk807 <luishk807@hotmail.com>
2020-11-26 13:20:17 -0500

    more testing

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use('/', require('./routes/userWishlists'));
 app.use('/', require('./routes/vendorRates'));
 app.use('/', require('./routes/banners'));
 app.use('/', require('./routes/bannerTypes'));
-// app.use('/', require('./routes/bannerImages'));
+app.use('/', require('./routes/bannerImages'));
 // app.use('/', require('./routes/sweetBoxes'));
 // app.use('/', require('./routes/sweetBoxProducts'));
 
