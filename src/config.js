ed0b6a8bb359fc4dbb4eaba9e6118afe4cf4060e
Luis Ho <evil_luis@hotmail.com>
2020-12-12 12:23:53 -0500

    fix sweetboxtype and whitelist

--- src/config.js
+++ src/config.js
@@ -7,5 +7,5 @@ dotenv.config();
 
 config.express = {
   port: process.env.PORT || 5000,
-  whitelist: ['http://localhost:3000','http://localhost:8080','https://avenidaz.herokuapp.com/'],
+  whitelist: ['http://localhost:3000','http://localhost:8080','https://avenidaz.herokuapp.com/', 'https://dev.avenidaz.com', 'https://avenidaz.com'],
 }
