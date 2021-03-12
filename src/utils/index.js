f7006f93c3c6ecad5a43b50e6e95aadf2fef4851
Steven <stevenova dstevey@hotmail.com>
2021-03-11 20:29:15 -0500

    CSV import format change. Product and variant import update

--- src/utils/index.js
+++ src/utils/index.js
@@ -12,10 +12,14 @@ const getDistinctValues = (dataArray, field) => {
     const output = [];
     dataArray.forEach(d => {
       // If it does not exist on the check object, then set flag to true
-      const stringValue = d[field].toString();
-      if (!checks[stringValue]) {
-        checks[stringValue] = true;
-        output.push(d[field]);
+      try {
+        const stringValue = d[field].toString();
+        if (!checks[stringValue]) {
+          checks[stringValue] = true;
+          output.push(d[field]);
+        }
+      } catch (err) {
+        logger.error(err);
       }
     });
     return output;
@@ -23,8 +27,38 @@ const getDistinctValues = (dataArray, field) => {
   return []
 }
 
+/**
+ * Gets the unique object array filtered by the given field
+ * @param {*} data 
+ */
+const getUniqueValuesByField = (dataArray, field) => {
+  const uniqueValueByField = getDistinctValues(dataArray, field);
+  const result = [];
+  uniqueValueByField.forEach(pname => {
+    result.push(
+      dataArray.find(d => d[field] === pname)
+    )
+  });
+  return result;
+}
+
+/** Returns a boolean indicating if all provided fields exists */
+const existFields = (obj, fields) => {
+  if (obj) {
+      for (let n = 0; n < fields.length; ++n) {
+          if (!obj[fields[n]]) {
+              return false;
+          }
+      }
+      return true;
+  } else {
+      return false;
+  }
+}
 
 module.exports = {
   cleanData,
-  getDistinctValues
+  getDistinctValues,
+  getUniqueValuesByField,
+  existFields
 }
