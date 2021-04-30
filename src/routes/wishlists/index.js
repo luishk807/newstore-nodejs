fa557d745303ef2a25ac8e12658fa4c98a6e0c2c
luishk807 <luishk807@hotmail.com>
2021-04-30 09:10:50 -0400

    AV-87 - fix wishlist

--- src/routes/wishlists/index.js
+++ src/routes/wishlists/index.js
@@ -1,68 +1,55 @@
 const router = require('express').Router();
 const cors = require('cors');
 const verify = require('../../middlewares/verifyToken');
-const UserWishlist = require('../../pg/models/UserWishlists');
+const controller = require('../../controllers/wishlists');
 const parser = require('../../middlewares/multerParser');
+
 
 router.all('*', cors());
 
-router.delete('/:id', verify, (req, res, next) => {
-  // delete brands
-  UserWishlist.findOne({ where: {id: req.params.id}})
-  .then((comment) => {
-    UserWishlist.destroy({
-      where: {
-        id: comment.id
-      }
-    }).then((deletedRecord) => {
-      if (deletedRecord) {
-        res.status(200).json({ status: true, message: "Wishlist successfully deleted" });
-      } else {
-        res.status(400).json({ status: false, message: "Error on deleting Wishlist!", error: e.toString(), req: req.body });
-      }
-    }, (err) => {
-        res.status(500).json({status: false, message: err});
-    })
-  })
-});
-
-
-router.put('/:id', [verify, parser.none()], (req, res, next) => {
-  const body = req.body;
-  const id = req.params.id;
-  const user = req.user.id;
-  UserWishlist.update(
-    {
-      'product': body.product,
-      'user': user,
-      'status': body.status
-    },
-    {
-      where: {
-        id: id
-      }
+router.delete('/:id', verify, async(req, res, next) => {
+  // delete wishlist
+  try {
+    const wishlist = await controller.deleteWishlist(req.params.id);
+    if (wishlist) {
+      res.status(200).json({ status: true, message: "Wishlist successfully deleted" });
+    } else {
+      res.status(400).json({ status: false, message: "Error on deleting Wishlist!", error: e.toString(), req: req.body });
     }
-  ).then((updated) => {
-    res.status(200).json({
-      data: updated,
-      message: "Rate Updated"
-    });
-  }).catch((err) => {
-    res.status(500).json({status: false, message: err})
-  })
+  } catch(err) {
+    res.status(500).json({status: false, message: err});
+  }
+
 });
 
-router.post('/', [verify, parser.none()], (req, res, next) => {
-  const body = req.body;
-  const user = req.user.id;
-  UserWishlist.create({
-    'product': body.productId,
-    'user': user
-  }).then((data) => {
-    res.status(200).json({status: true, message: 'Success: Wishlist Added', data: data});
-  }).catch((err) => {
-    res.status(500).json({status: false, message: err})
-  })
+
+router.put('/:id', [verify, parser.none()], async(req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    const wishlist = await controller.updateWishlist(req, id);
+    if (wishlist) {
+      res.status(200).json({ status: true, message: "Wishlist successfully updated" });
+    } else {
+      res.status(400).json({ status: false, message: "Error on updated Wishlist!", error: e.toString(), req: req.body });
+    }
+  } catch(err) {
+    res.status(500).json({status: false, message: err});
+  }
+
+});
+
+router.post('/', [verify, parser.none()], async(req, res, next) => {  
+  try {
+    const wishlist = await controller.createWishlist(req);
+    if (wishlist) {
+      res.status(200).json({ status: true, message: "Wishlist successfully created" });
+    } else {
+      res.status(400).json({ status: false, message: "Error on created Wishlist!", error: e.toString(), req: req.body });
+    }
+  } catch(err) {
+    res.status(500).json({status: false, message: err});
+  }
 })
 
 router.get('/', async(req, res, next) => {
@@ -70,14 +57,14 @@ router.get('/', async(req, res, next) => {
   let data = null;
   if (req.query.id) {
     try {
-      data = await UserWishlist.findOne({ where: {id: req.query.id}});
+      data = await controller.getWishlistById(req.query.id);
       res.json(data)
     } catch(err) {
       res.send({status: false, message: err})
     }
   } else {
     try {
-      data = await UserWishlist.findAll();
+      data = await controller.getAllWishlist();
       res.json(data)
     } catch(err) {
       res.send({status: false, message: err})
@@ -85,26 +72,18 @@ router.get('/', async(req, res, next) => {
   }
 });
 
-router.delete('/user/:id', verify, (req, res, next) => {
-  // delete brands
-  const user = req.user.id;
-
-  UserWishlist.findOne({ where: {user: user, product: req.params.id}})
-  .then((data) => {
-    UserWishlist.destroy({
-      where: {
-        id: data.id
-      }
-    }).then((deletedRecord) => {
-      if (deletedRecord) {
-        res.status(200).json({ status: true, message: "Wishlist successfully deleted" });
-      } else {
-        res.status(400).json({ status: false, message: "Error on deleting Wishlist!", error: e.toString(), req: req.body });
-      }
-    }, (err) => {
-        res.status(500).json({status: false, message: err});
-    })
-  })
+router.delete('/user/:id', verify, async(req, res, next) => {
+  // delete wishlist
+  try {
+    const wishlist = await controller.deleteUserWishlist(req);
+    if (wishlist) {
+      res.status(200).json({ status: true, message: "Wishlist successfully deleted" });
+    } else {
+      res.status(400).json({ status: false, message: "Error on deleting Wishlist!", error: e.toString(), req: req.body });
+    }
+  } catch(err) {
+    res.status(500).json({status: false, message: err});
+  }
 });
 
 router.get('/user', [verify], async(req, res, next) => {
@@ -112,14 +91,14 @@ router.get('/user', [verify], async(req, res, next) => {
   const user = req.user.id;
   if (req.query.product) {
     try {
-      data = await UserWishlist.findOne({where: {user: user, product: req.query.product}});
+      data = await controller.getUserWishlistById(req);
       res.json(data)
     } catch(err) {
       res.send({status: false, message: err})
     }
   } else {
     try {
-      data = await UserWishlist.findAll({ where: {user: user}, include: ['wishlistProduct','userWishlistUser','userWishlistStatus']});
+      data = await controller.getAllWishlistByUser(user);
       res.json(data)
     } catch(err) {
       res.send({status: false, message: err})
