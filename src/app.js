d87650f497a8a6e239f7e2c20986269ecf70f6b4
luishk807 <luishk807@hotmail.com>
2021-05-31 16:23:58 -0400

    enforce ssl

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const configOAuth = require('./oauth');
-
+const redirectSSL = require('redirect-ssl');
 // const cors = require('cors');
 const routesLoader = require('./routes-loader');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true } ));
 app.use(bodyParser.json({ limit: '5mb' }));
+
+// Add middleware
+app.use(redirectSSL.create({
+  exclude: ['localhost'],
+  enabled: process.env.NODE_ENV === 'production'
+}));
 
 // Still need to configure app.oauth.authorize() middleware in routes, seems like tricket to make this app.oauth.authoerize() available in routes
 // configOAuth(app);
