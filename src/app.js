ddcee50dd69eb428957f7d60d44c72f46cd95a35
luishk807 <luishk807@hotmail.com>
2021-05-31 17:39:45 -0400

    enforce ssl

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const configOAuth = require('./oauth');
-const redirectSSL = require('redirect-ssl');
+const sslRedirect = require('heroku-ssl-redirect').default;
 // const cors = require('cors');
 const routesLoader = require('./routes-loader');
 const app = express();
@@ -9,14 +9,19 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true } ));
 app.use(bodyParser.json({ limit: '5mb' }));
 
-// Add middleware
-app.use(redirectSSL.create({
-  exclude: ['localhost'],
-  enabled: process.env.NODE_ENV === 'production'
-}));
+// enable ssl redirect
+app.use(sslRedirect());
 
 // Still need to configure app.oauth.authorize() middleware in routes, seems like tricket to make this app.oauth.authoerize() available in routes
 // configOAuth(app);
+app.use(sslRedirect(['production'], 301));
+
+// set up a route to redirect http to https
+if (process.env.NODE_ENV === "production") {
+  app.get('*', (request, response) => {
+    response.redirect('https://' + request.headers.host + request.url);
+  });
+}
 
 routesLoader(app, './routes');
 // app.use(cors());
