7b6c041ad68d6da0dfa87958f57a9a9daf8fe10e
luishk807 <luishk807@hotmail.com>
2022-01-22 15:09:11 -0500

    integrate migrations

---
+++ config/config.js
@@ -0,0 +1,18 @@
+require('dotenv').config(); // this is important!
+
+module.exports = {
+  development: {
+    "username": process.env.PGUSER,
+    "password": process.env.PGPASSWORD,
+    "database": process.env.PGDATABASE,
+    "host": process.env.PGHOST,
+    "dialect": "postgres"
+  },
+  production: {
+    "username": process.env.PGUSER,
+    "password": process.env.PGPASSWORD,
+    "database": process.env.PGDATABASE,
+    "host": process.env.PGHOST,
+    "dialect": "postgres"
+  }
+};
